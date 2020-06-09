e90ded4c9dd6ad58891e8072cf5f5a895d7253ef
Ashok Kumar Reddy <kummethashokumareddy@gmail.com>
2020-06-09 16:59:13 +0530

    code chef

--- Jun Long Term/problem 2.py
+++ Jun Long Term/problem 2.py
@@ -2,20 +2,7 @@ def process(l,pair):
     if len(l) <= 1 :
         print(pair)
         return 0
-    else:
-        a = l[0]
-        l = l[1:]
-        if a == 'x' and l[0] == 'y':
-            pair += 1
-            process(l[1:],pair)
-            return 
-        elif a == 'y' and l[0] == 'x':
-            pair += 1
-            process(l[1:],pair)
-            return
-        else:
-            process(l,pair)
-            return
+    
 
 
 
