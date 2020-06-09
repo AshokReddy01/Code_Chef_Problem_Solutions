5a5db793d72e8c4e7504964d656f8b1bf174bb24
Ashok Kumar Reddy <kummethashokumareddy@gmail.com>
2020-06-09 17:14:32 +0530

    commit

--- Jun Long Term/problem 2.py
+++ Jun Long Term/problem 2.py
@@ -1,8 +1,31 @@
-def process(l,pair):
-    if len(l) <= 1 :
-        print(pair)
-        return 0
-    
+'''
+Chef and String 
+
+Problem Code: XYSTR
+
+'''
+
+
+def process(data):
+    pairs_found = 0
+    free_pass = False
+    first_persion = None
+    for i in data:
+        if free_pass:
+            first_persion = i
+            free_pass = False
+            continue
+        if first_persion == None:
+            first_persion = i
+            continue
+        if (first_persion == 'x' and i == 'y') or (first_persion == 'y' and i == 'x'):
+            free_pass = True
+            pairs_found +=1
+    return pairs_found
+
+            
+
+
 
 
 
@@ -10,4 +33,4 @@ if __name__ == '__main__':
     for test in range(int(input())):
         string = input()
 
-        process(string,pair=0)
+        print(process(string))
