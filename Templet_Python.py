3dedaa5e12098b73ce6407eb1efc39fb7a2e276c
AshoKumaReddy <39928813+AshokReddy01@users.noreply.github.com>
2020-06-11 19:29:51 +0530

    templet

---
+++ Templet_Python.py
@@ -0,0 +1,13 @@
+from sys import stdin
+input = stdin.readline
+from collections import Counter as count_values
+
+def source():
+    pass
+
+
+if __name__ == "__main__":
+    
+    for test in range(int(input())):
+        value = source()
+        print(value)
